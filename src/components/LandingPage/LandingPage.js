6c6c28decc785f09e994ea72a0d4c02e25419cf4
Amar Adloid <amar.k@adloid.in>
2022-05-21 20:39:44 +0530

    cmmted

---
+++ src/components/LandingPage/LandingPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './index.css';
+import backGroundImage from "../../assets/backGroundImage.svg";
+export default function LandingPage() {
+  return (
+    <div className='container'>
+      <div className='top-panel'>
+        <img src="backGroundImage">
+        </img>
+      </div>
+    </div>
+  )
+}
