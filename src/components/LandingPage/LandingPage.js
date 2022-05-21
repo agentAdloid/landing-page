8f2dbf3ed60b608296b6c0755e1dba7c52c9cfd3
Amar Adloid <amar.k@adloid.in>
2022-05-21 21:59:43 +0530

    added contact buttons

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import './index.css';
+import React from "react";
+import "./index.css";
 import backGroundImage from "../../assets/backGroundImage.svg";
 import polyGon from "../../assets/whitePolygon.svg";
 import Metadome from "../../assets/Metadome.svg";
@@ -7,17 +7,35 @@ import KanavsImage from "../../assets/KanavsImage.svg";
 
 export default function LandingPage() {
   return (
-    <div className='container'>
-      <div className='top-panel'>
-        <img src={backGroundImage} className='cover-screen-img'>
-        </img>
-        <img src={polyGon} className={'polyGon ' + 'cover-screen'}>
-        </img>
-        <img src={Metadome} className={'cover-screen ' + 'logo'}>
-        </img>
-        <img src={KanavsImage} className={'kanavs'}>
-        </img>
+    <div className="container">
+      <div className="top-panel">
+        <img src={backGroundImage} className="cover-screen-img"></img>
+        <img src={polyGon} className={"polyGon " + "cover-screen"}></img>
+        <img src={Metadome} className={"cover-screen " + "logo"}></img>
+        <img src={KanavsImage} className={"kanavs"}></img>
+      </div>
+      <div className="bottom-panel">
+        <div className="intro">
+          <p className="name">Hi, I’m Kanav Singla</p>
+          <p className="foundingInfo">Founder & CEO, Metadome.ai</p>
+          <p className="intro-details">
+          An inventor in the XR space dedicated <br/>
+          to making immersive tech more accessible.
+          </p>
+          <div className="contact-row ">
+            <div className="contact-button center">
+              Follow
+            </div>
+            <div className="contact-button center">
+              Email
+            </div>
+            <div className="contact-button center">
+              Call
+            </div>
+            
+          </div>
+        </div>
       </div>
     </div>
-  )
+  );
 }
